d9ab405d7adc5bf0bbb3ee08846a0fd5b148da06
Red0699 <bajistamar@gmail.com>
2024-05-18 05:09:00 -0500

    Se agrega como encabezado el token leyendo la Cookie

--- src/app/services/csrf-interceptor.service.ts
+++ src/app/services/csrf-interceptor.service.ts
@@ -6,10 +6,37 @@ import { Observable } from 'rxjs';
 export class CsrfInterceptor implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    // Lee el token CSRF de las cookies
+    const csrfToken = this.getCookie('XSRF-TOKEN');
+
+    // Si el token CSRF existe, clona la solicitud y establece el encabezado 'X-XSRF-TOKEN'
+    if (csrfToken) {
+      req = req.clone({
+        headers: req.headers.set('X-XSRF-TOKEN', csrfToken)
+      });
+    }
+
     // Clona la solicitud y establece la propiedad withCredentials en true
     const clonedReq = req.clone({ withCredentials: true });
 
     // Maneja la solicitud modificada
     return next.handle(clonedReq);
+  }
+
+  // Función para leer una cookie
+  getCookie(name: string): string {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+      const cookies = document.cookie.split(';');
+      for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].trim();
+        
+        if (cookie.substring(0, name.length + 1) === (name + '=')) {
+          cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+          break;
+        }
+      }
+    }
+    return cookieValue;
   }
 }
